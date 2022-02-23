74326a19f8f072e20f22056ff65cc01da2f818de
InKryption <inkryption07@gmail.com>
2022-02-23 01:03:05 +0100

    More progress; added different init functions based on a comptime-able version, removing the original. More getter functions. Advance function should be sound.

--- src/main.zig
+++ src/main.zig
@@ -8,65 +8,162 @@ pub fn main() !void {
     var default_prng = std.rand.DefaultPrng.init(@bitCast(u64, std.time.milliTimestamp()));
     const random = default_prng.random();
 
-    var sg = try SnakeGame.init(allocator, random, .{ .w = 10, .h = 10 });
-    defer sg.deinit();
+    var sg = try SnakeGame.initAllocRandom(allocator, .{ .w = 10, .h = 10 }, random);
+    defer sg.deinitAllocated(allocator);
 }
 
 const SnakeGame = struct {
-    allocator: std.mem.Allocator,
-    grid: OccupantGrid,
+    occupants: [*]Occupant,
+    size: Indexer.Bounds,
     snake_head_coord: Indexer.Coord,
     snake_tail_coord: Indexer.Coord,
 
     pub const OccupantGrid = Grid(Occupant);
     pub const Indexer = OccupantGrid.Indexer;
 
-    pub fn init(allocator: std.mem.Allocator, random: std.rand.Random, size: Indexer.Bounds) !SnakeGame {
-        const grid = try OccupantGrid.init(allocator, size, .air);
-        errdefer grid.deinit(allocator);
-
-        const snake_head_coord = Indexer.Coord{
-            .x = random.uintLessThan(Indexer.HalfUInt, grid.size.w),
-            .y = random.uintLessThan(Indexer.HalfUInt, grid.size.h),
-        };
-
-        grid.getPtr(snake_head_coord).item = @unionInit(Occupant, "snake", .{
-            .direction = random.enumValue(Direction),
-            .rotation = if (random.boolean()) random.enumValue(Rotation) else null,
-        });
-
-        const snake_tail_coord: Indexer.Coord = Indexer.coordPlusOffsetWrapped(
+    /// A combination of `initAlloc` and `initRandom`.
+    /// Must be deinitialized with `deinitAllocated`.
+    pub fn initAllocRandom(allocator: std.mem.Allocator, size: Indexer.Bounds, random: std.rand.Random) std.mem.Allocator.Error!SnakeGame {
+        return try initAlloc(
+            allocator,
             size,
-            snake_head_coord,
-            Indexer.directionOffset(grid.getPtr(snake_head_coord).item.snake.direction.reversed()),
+            Indexer.Coord{
+                .x = random.uintAtMost(Indexer.HalfUInt, size.w),
+                .y = random.uintAtMost(Indexer.HalfUInt, size.h),
+            },
+            SnakeCellData{
+                .direction = random.enumValue(Direction),
+                .rotation = if (random.boolean()) random.enumValue(Rotation) else null,
+            },
         );
-
-        return SnakeGame{
-            .allocator = allocator,
-            .grid = grid,
-            .snake_head_coord = snake_head_coord,
-            .snake_tail_coord = snake_tail_coord,
-        };
     }
 
-    pub fn deinit(self: SnakeGame) void {
-        self.grid.deinit(self.allocator);
+    /// Allocates a buffer of the appropriate size.
+    /// Must be deinitialized with `deinitAllocated`.
+    pub fn initAlloc(allocator: std.mem.Allocator, size: Indexer.Bounds, head_coord: Indexer.Coord, head_data: SnakeCellData) std.mem.Allocator.Error!SnakeGame {
+        const buffer = try allocator.alloc(Occupant, Indexer.boundsToLen(size));
+        errdefer allocator.free(buffer);
+
+        return initBuffer(buffer, size, head_coord, head_data) catch unreachable;
+    }
+
+    pub fn deinitAllocated(self: SnakeGame, allocator: std.mem.Allocator) void {
+        allocator.free(self.getOccupantsSliceMut());
+    }
+
+    /// Same as `initBuffer`, but snake spawns at a randomly determined location.
+    pub fn initBufferRandom(buffer: []Occupant, size: Indexer.Bounds, random: std.rand.Random) Indexer.Error!SnakeGame {
+        return try initBuffer(
+            buffer,
+            size,
+            Indexer.Coord{
+                .x = random.uintAtMost(Indexer.HalfUInt, size.w),
+                .y = random.uintAtMost(Indexer.HalfUInt, size.h),
+            },
+            SnakeCellData{
+                .direction = random.enumValue(Direction),
+                .rotation = if (random.boolean()) random.enumValue(Rotation) else null,
+            },
+        );
+    }
+
+    /// Asserts that the legnth of the buffer given is equivalent to the length represented by the size given.
+    /// Buffer must remain alive for the duration of the life of this snake.
+    /// Deinitialization not required.
+    pub fn initBuffer(buffer: []Occupant, size: Indexer.Bounds, head_coord: Indexer.Coord, head_data: SnakeCellData) Indexer.Error!SnakeGame {
+        std.debug.assert(Indexer.boundsToLen(size) == buffer.len);
+        const head_index = try Indexer.coordToIndexInBounds(size, head_coord);
+
+        std.mem.set(Occupant, buffer, .air);
+        buffer[head_index] = @unionInit(Occupant, "snake", head_data);
+
+        const tail_coord = Indexer.coordPlusOffsetWrapped(
+            size,
+            head_coord,
+            Indexer.directionOffset(buffer[head_index].snake.direction.reversed()),
+        );
+        buffer[Indexer.coordToIndexInBounds(size, tail_coord) catch unreachable] = @unionInit(Occupant, "snake", .{
+            .direction = buffer[head_index].snake.direction,
+            .rotation = null,
+        });
+
+        return SnakeGame{
+            .occupants = buffer.ptr,
+            .size = size,
+            .snake_head_coord = head_coord,
+            .snake_tail_coord = tail_coord,
+        };
     }
 
     pub fn advance(self: *SnakeGame) Event {
-        const old_head_coord = self.snake_head_coord;
-        const old_tail_coord = self.snake_tail_coord;
-        
-        const old_head_data = self.snakeHeadCellPtr().*;
-        const old_tail_data = self.snakeTailCellPtr().*;
+        const old_head_coord: Indexer.Coord = self.snake_head_coord;
+        const old_tail_coord: Indexer.Coord = self.snake_tail_coord;
+
+        const old_head_data: SnakeCellData = self.getSnakeHeadCell().snake;
+        const old_tail_data: SnakeCellData = self.getSnakeTailCell().snake;
+
+        const old_head_direction: Direction = if (old_head_data.rotation) |r| old_head_data.direction.rotated(r) else old_head_data.direction;
+        const old_tail_direction: Direction = if (old_tail_data.rotation) |r| old_tail_data.direction.rotated(r) else old_tail_data.direction;
+
+        const dst_head_coord: Indexer.Coord = Indexer.coordPlusOffsetWrapped(self.size, old_head_coord, Indexer.directionOffset(old_head_direction));
+        const dst_tail_coord: Indexer.Coord = Indexer.coordPlusOffsetWrapped(self.size, old_tail_coord, Indexer.directionOffset(old_tail_direction));
+
+        std.debug.assert(self.getOccupant(dst_tail_coord) == .snake);
+        switch (self.getOccupant(dst_head_coord)) {
+            .food => {},
+            .air, .snake => {
+                self.getSnakeTailCellPtr().* = .air;
+                self.snake_tail_coord = dst_tail_coord;
+            },
+        }
+
+        switch (self.getOccupant(dst_head_coord)) {
+            .snake => {},
+            .air, .food => {
+                self.snake_head_coord = dst_head_coord;
+                self.getSnakeHeadCellPtr().* = @unionInit(Occupant, "snake", .{
+                    .direction = old_head_direction,
+                    .rotation = null,
+                });
+            },
+        }
+
+        return switch (self.getOccupant(dst_head_coord)) {
+            .air => .move,
+            .food => .grow,
+            .snake => .{ .collision = undefined },
+        };
     }
 
-    fn snakeHeadCellPtr(self: *SnakeGame) *OccupantGrid.Cell {
-        return self.grid.getPtr(self.snake_head_coord);
+    pub fn getSnakeHeadCell(self: SnakeGame) Occupant {
+        return self.getOccupant(self.snake_head_coord);
+    }
+    pub fn getSnakeHeadCellPtr(self: *SnakeGame) *Occupant {
+        return self.getOccupantPtr(self.snake_head_coord);
     }
 
-    fn snakeTailCellPtr(self: *SnakeGame) *OccupantGrid.Cell {
-        return self.grid.getPtr(self.snake_tail_coord);
+    pub fn getSnakeTailCell(self: SnakeGame) Occupant {
+        return self.getOccupant(self.snake_tail_coord);
+    }
+    pub fn getSnakeTailCellPtr(self: *SnakeGame) *Occupant {
+        return self.getOccupantPtr(self.snake_tail_coord);
+    }
+
+    pub fn getOccupant(self: SnakeGame, coord: Indexer.Coord) Occupant {
+        var copy = self;
+        return copy.getOccupantPtr(coord).*;
+    }
+    pub fn getOccupantPtr(self: *SnakeGame, coord: Indexer.Coord) *Occupant {
+        const index = Indexer.coordToIndexInBounds(self.size, coord) catch unreachable;
+        return &self.getOccupantsSliceMut()[index];
+    }
+
+    pub fn getOccupantsSlice(self: SnakeGame) []const Occupant {
+        var copy = self;
+        return copy.getOccupantsSliceMut();
+    }
+    pub fn getOccupantsSliceMut(self: SnakeGame) []Occupant {
+        return self.occupants[0..Indexer.boundsToLen(self.size)];
     }
 
     pub const Event = union(enum) {
@@ -79,7 +176,7 @@ const SnakeGame = struct {
         };
     };
 
-    const Occupant = union(enum) {
+    pub const Occupant = union(enum) {
         air,
         food,
         snake: SnakeCellData,
