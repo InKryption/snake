9eac5a9ab9decac7d107c39f904c0f0fb64f3f33
InKryption <inkryption07@gmail.com>
2022-02-25 23:46:36 +0100

    Updates to logic here

--- src/spatial/indexer2d.zig
+++ src/spatial/indexer2d.zig
@@ -9,44 +9,104 @@ pub fn Indexer2d(comptime int_bits: u16) type {
         pub const FullSint = std.meta.Int(.signed, int_bits);
         pub const HalfSInt = if (int_bits % 2 == 0) std.meta.Int(.signed, @divExact(int_bits, 2)) else Indexer2d(int_bits - 1).HalfSInt;
 
-        pub const Bounds = struct { w: HalfUInt, h: HalfUInt };
-        pub const Coord = struct { x: HalfUInt, y: HalfUInt };
-        pub const Offset = struct { x: HalfSInt, y: HalfSInt };
+        pub const Bounds = struct {
+            w: HalfUInt,
+            h: HalfUInt,
 
-        pub const Error = error{OutOfBounds};
+            pub fn toLen(bounds: Bounds) FullUInt {
+                return boundsToLen(bounds);
+            }
+        };
+
+        pub const Coord = struct {
+            x: HalfUInt,
+            y: HalfUInt,
+
+            pub fn toIndexInBounds(coord: Coord, bounds: Bounds) OutOfBounds!FullUInt {
+                return coordToIndexInBounds(bounds, coord);
+            }
+
+            pub fn toIndexInOrOnBounds(coord: Coord, bounds: Bounds) OutOfBounds!FullUInt {
+                return coordToIndexInOrOnBounds(bounds, coord);
+            }
+
+            pub fn plusOffsetClamped(coord: Coord, bounds: Bounds, offset: Offset) Coord {
+                return coordPlusOffsetClamped(bounds, coord, offset);
+            }
+
+            pub fn plusOffsetWrapped(coord: Coord, bounds: Bounds, offset: Offset) Coord {
+                return coordPlusOffsetWrapped(bounds, coord, offset);
+            }
+        };
+
+        pub const Offset = struct {
+            x: HalfSInt,
+            y: HalfSInt,
+
+            pub fn add(a: Offset, b: Offset) Offset {
+                return addOffsets(a, b);
+            }
+        };
+
+        pub const OutOfBounds = error{OutOfBounds};
 
         pub fn boundsToLen(bounds: Bounds) FullUInt {
             return std.math.mulWide(HalfUInt, bounds.w, bounds.h);
         }
 
-        pub fn coordToIndexInBounds(bounds: Bounds, coord: Coord) Error!FullUInt {
-            const result = @as(FullUInt, coord.x) + std.math.mulWide(HalfUInt, coord.y, bounds.w);
-            return if ((coord.x < bounds.w or coord.y < bounds.h) and result < boundsToLen(bounds))
-                result
-            else
-                error.OutOfBounds;
+        fn coordToIndexUnsafe(bounds: Bounds, coord: Coord) FullUInt {
+            return @as(FullUInt, coord.x) + std.math.mulWide(HalfUInt, coord.y, bounds.w);
         }
 
-        pub fn coordToIndexInOrOnBounds(bounds: Bounds, coord: Coord) Error!FullUInt {
-            const result = @as(FullUInt, coord.x) + std.math.mulWide(HalfUInt, coord.y, bounds.w);
-            return if ((coord.x <= bounds.w or coord.y <= bounds.h) and result <= boundsToLen(bounds))
-                result
-            else
-                error.OutOfBounds;
+        pub fn coordIsInBounds(bounds: Bounds, coord: Coord) bool {
+            return (coord.x < bounds.w and coord.y < bounds.h);
         }
 
-        pub fn indexToCoordInBounds(bounds: Bounds, index: FullUInt) Error!Coord {
-            return if (index < boundsToLen(bounds)) Coord{
+        pub fn coordToIndexInBounds(bounds: Bounds, coord: Coord) OutOfBounds!FullUInt {
+            return if (coordIsInBounds(bounds, coord))
+                coordToIndexUnsafe(bounds, coord)
+            else
+                OutOfBounds.OutOfBounds;
+        }
+
+        pub fn coordIsInOrOnBounds(bounds: Bounds, coord: Coord) bool {
+            return (coord.x <= bounds.w and coord.y <= bounds.h);
+        }
+
+        pub fn coordToIndexInOrOnBounds(bounds: Bounds, coord: Coord) OutOfBounds!FullUInt {
+            return if (coordIsInOrOnBounds(bounds, coord))
+                coordToIndexUnsafe(bounds, coord)
+            else
+                OutOfBounds.OutOfBounds;
+        }
+
+        fn indexToCoordUnsafe(bounds: Bounds, index: FullUInt) Coord {
+            return Coord{
                 .x = @intCast(HalfUInt, index % bounds.w),
                 .y = @intCast(HalfUInt, @divTrunc(index, bounds.w)),
-            } else error.OutOfBounds;
+            };
         }
 
-        pub fn indexToCoordInOrOnBounds(bounds: Bounds, index: FullUInt) Error!Coord {
-            return if (index <= boundsToLen(bounds)) Coord{
-                .x = index % bounds.w,
-                .y = @divTrunc(index, bounds.w),
-            } else error.OutOfBounds;
+        pub fn indexIsInBounds(bounds: Bounds, index: FullUInt) bool {
+            return index < boundsToLen(bounds);
+        }
+
+        pub fn indexToCoordInBounds(bounds: Bounds, index: FullUInt) OutOfBounds!Coord {
+            return if (indexIsInBounds(bounds, index))
+                indexToCoordUnsafe(bounds, index)
+            else
+                error.OutOfBounds;
+        }
+
+        pub fn indexIsInOrOnBounds(bounds: Bounds, index: FullUInt) bool {
+            return index <= boundsToLen(bounds);
+        }
+
+        pub fn indexToCoordInOrOnBounds(bounds: Bounds, index: FullUInt) OutOfBounds!Coord {
+            return if (indexIsInOrOnBounds(bounds, index))
+                indexToCoordUnsafe(bounds, index)
+            else
+                error.OutOfBounds;
         }
 
         pub fn directionOffset(direction: spatial.Direction) Offset {
--- src/spatial/spatial.zig
+++ src/spatial/spatial.zig
@@ -42,3 +42,65 @@ pub const Direction = enum {
     }
 };
 
+pub fn directionPlusOptionalRotation(
+    direction: Direction,
+    maybe_rotation: ?Rotation,
+) DirectionPlusOptionalRotation {
+    return DirectionPlusOptionalRotation.from(direction, maybe_rotation);
+}
+
+pub const DirectionPlusOptionalRotation = enum {
+    north,
+    north_clockwise,
+    north_anticlockwise,
+
+    east,
+    east_clockwise,
+    east_anticlockwise,
+
+    south,
+    south_clockwise,
+    south_anticlockwise,
+
+    west,
+    west_clockwise,
+    west_anticlockwise,
+
+    pub fn from(d: Direction, maybe_rotation: ?Rotation) DirectionPlusOptionalRotation {
+        return if (maybe_rotation) |rotation| switch (d) {
+            .north => @as(@This(), switch (rotation) {
+                .clockwise => .north_clockwise,
+                .anticlockwise => .north_anticlockwise,
+            }),
+            .east => @as(@This(), switch (rotation) {
+                .clockwise => .east_clockwise,
+                .anticlockwise => .east_anticlockwise,
+            }),
+            .south => @as(@This(), switch (rotation) {
+                .clockwise => .south_clockwise,
+                .anticlockwise => .south_anticlockwise,
+            }),
+            .west => @as(@This(), switch (rotation) {
+                .clockwise => .west_clockwise,
+                .anticlockwise => .west_anticlockwise,
+            }),
+        } else @as(@This(), switch (d) {
+            .north => .north,
+            .east => .east,
+            .south => .south,
+            .west => .west,
+        });
+    }
+
+    pub fn direction(self: DirectionPlusOptionalRotation) Direction {
+        return switch (self) {
+            .north, .north_clockwise, .north_anticlockwise => .north,
+
+            .east, .east_clockwise, .east_anticlockwise => .east,
+
+            .south, .south_clockwise, .south_anticlockwise => .south,
+
+            .west, .west_clockwise, .west_anticlockwise => .west,
+        };
+    }
+};
